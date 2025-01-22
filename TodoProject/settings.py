32e539697a28c4ef28bf769397a03c9be3e0b004
khetumewada <khetu.mewada@gmail.com>
2025-01-22 20:01:55 +0530

    done vercel setting

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--59lr29i$l09lt(hq+$p3z&292#omio7jlg!!n0k+!bdaxe)bc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
 
 
 # Application definition
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
